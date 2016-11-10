a08c997f0163be6b16b249b6a569ee18054981bb
Nick Ng <ngyewch@gmail.com>
2016-11-11 00:08:13 +0800

    Updated example home.

--- web/HomeComponent.dart
+++ web/HomeComponent.dart
@@ -2,13 +2,12 @@
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
 import 'package:angular2/core.dart';
-
-import 'package:semantic_ui_angular_dart/semantic_ui_angular_dart.dart';
+import 'package:angular2/router.dart';
 
 @Component(
     selector: 'home',
     templateUrl: 'HomeComponent.html',
-    directives: const [SEMANTIC_UI_DIRECTIVES]
+    directives: const [ROUTER_DIRECTIVES]
 )
 class HomeComponent {
 }
