30348e27abe0a6103da0c41b11181506bc1c7637
Nick Ng <ngyewch@gmail.com>
2016-11-13 19:14:53 +0800

    Cleanup.

--- web/DropdownComponent.dart
+++ web/DropdownComponent.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2016, Nick Ng. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/core.dart';
 
 import 'package:semantic_ui_angular_dart/semantic_ui_angular_dart.dart';
--- web/main.dart
+++ web/main.dart
@@ -1,6 +1,3 @@
-// Copyright (c) 2016, Nick Ng. All rights reserved. Use of this source code
-// is governed by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:angular2/core.dart';
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/platform/common.dart';
